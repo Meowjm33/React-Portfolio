2182138c5386f915017a95fd1de1610b83515171
surangana thapa <shilpithapa233@gmail.com>
2026-01-08 11:34:59 +0545

    My Portfolio

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+///Vite le react import garna parcha so mandatory cha yo!!!!!
+
+const Sidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
